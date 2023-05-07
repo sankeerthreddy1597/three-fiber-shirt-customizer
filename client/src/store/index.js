5ad24ddc32d7bf49b8e02fb2967e0f5b49978e2b
sankeerthreddy1597 <54692080+sankeerthreddy1597@users.noreply.github.com>
2023-05-07 16:29:29 -0700

    implement color picker

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,7 +2,7 @@ import { proxy } from "valtio";
 
 const state = proxy({
   intro: true,
-  color: "#3ADED8",
+  color: "#E88631",
   isLogoTexture: true,
   isFullTexture: false,
   logoDecal: "./threejs.png",
