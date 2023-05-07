a8b8f68ed5d7bdc01163e714fa957294bdcdc647
sankeerthreddy1597 <54692080+sankeerthreddy1597@users.noreply.github.com>
2023-05-07 12:16:59 -0700

    setup back button in customizor page

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -17,7 +17,41 @@ import {
 } from "../components";
 
 const Customizer = () => {
-  return <div>Customizer</div>;
+  const snap = useSnapshot(state);
+
+  return (
+    <AnimatePresence>
+      {!snap.intro && (
+        <>
+          <motion.div
+            key="motion"
+            className="absolute top-0 left-0 z-10"
+            {...slideAnimation("left")}
+          >
+            <div className="flex items-center min-h-screen">
+              <div className="editortabs-container tabs">
+                {EditorTabs.map((tab) => (
+                  <Tab key={tab.name} tab={tab} handleClick={() => {}} />
+                ))}
+              </div>
+            </div>
+          </motion.div>
+
+          <motion.div
+            className="absolute z-10 top-5 right-5"
+            {...fadeAnimation}
+          >
+            <CustomButton
+              type="filled"
+              title="Go Back"
+              handleClick={() => (state.intro = true)}
+              customerStyles="w-fit px-4 py-2.5 font-bold text-sm"
+            />
+          </motion.div>
+        </>
+      )}
+    </AnimatePresence>
+  );
 };
 
 export default Customizer;
