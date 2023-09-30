810d16aef89456a0ec06604dee6722eaf2fbfa89
sankeerthreddy1597 <54692080+sankeerthreddy1597@users.noreply.github.com>
2023-09-30 12:17:41 -0700

    update customizer

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -37,9 +37,28 @@ const Customizer = () => {
       case "filepicker":
         return <FilePicker file={file} setFile={setFile} readFile={readFile} />;
       case "aipicker":
-        return <AIPicker />;
+        return (
+          <AIPicker
+            prompt={prompt}
+            setPrompt={setPrompt}
+            generatingImg={generatingImg}
+            handleSubmit={handleSubmit}
+          />
+        );
       default:
         return null;
+    }
+  };
+
+  const handleSubmit = async (type) => {
+    if (!prompt) return alert("Please enter a prompt");
+
+    try {
+    } catch (error) {
+      alert(error.message);
+    } finally {
+      setGeneratingImg(false);
+      setActiveEditorTab("");
     }
   };
 
